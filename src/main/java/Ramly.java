c80a3e9eafe57c417f17d3bc8d45bd771cd7382a
N Sriram <narayanamurthysriram@gmail.com>
2026-08-20 01:48:55 +0800

    Added Todo,Deadline,Event class to have multiple classes

--- src/main/java/Ramly.java
+++ src/main/java/Ramly.java
@@ -15,7 +15,6 @@ public class Ramly {
         System.out.println(line);
 
         Task[] storage = new Task[100];
-        //String[] store = new String[100];
         int count = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -31,7 +30,7 @@ public class Ramly {
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                        System.out.println(i+1 + ". " + storage[i]);
+                        System.out.println(i+1 + "." + storage[i]);
                 }
                 System.out.println(line);
             } else if (input.startsWith("mark")) {
@@ -48,10 +47,30 @@ public class Ramly {
                 System.out.println(" " + storage[taskIndex]);
                 System.out.println(line);
 
-            } else {
-                storage[count] = new Task(input);
+            } else if (input.startsWith("todo")) {
+                storage[count] = new Todo(input.substring(5).trim());
                 count++;
-                System.out.println("added: " + input);
+                System.out.println("Received! I've added this task:");
+                System.out.println(" " + storage[count-1]);
+                System.out.println("Now you have " + count +  " tasks in the list.");
+                System.out.println(line);
+            } else if (input.startsWith("deadline")) {
+                String pure = input.substring(9); // "return book /by Sunday"
+                String[] parts = pure.split(" /by ", 2);
+                storage[count] = new Deadline(parts[0].trim(), parts[1].trim());
+                count++;
+                System.out.println("Received! I've added this task:");
+                System.out.println(" " + storage[count-1]);
+                System.out.println("Now you have " + count +  " tasks in the list.");
+                System.out.println(line);
+            } else if (input.startsWith("event")) {
+                String pure = input.substring(6); // "project meeting /from Mon 2pm /to 4pm"
+                String[] parts = pure.split(" /from | /to ");
+                storage[count] = new Event(parts[0].trim(), parts[1].trim(), parts[2].trim());
+                count++;
+                System.out.println("Received! I've added this task:");
+                System.out.println(" " + storage[count-1]);
+                System.out.println("Now you have " + count +  " tasks in the list.");
                 System.out.println(line);
             }
         }
