61480e93185d6514bec9f4d30d14bf1dcdadffee
N Sriram <narayanamurthysriram@gmail.com>
2026-08-19 23:33:52 +0800

    Added custom greeting and exit

--- src/main/java/Ramly.java
+++ src/main/java/Ramly.java
@@ -1,11 +1,19 @@
 public class Ramly {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = " ____                 _       \n"
                 + "|  _ \\ __ _ _ __ ___ | |_   _ \n"
                 + "| |_) / _` | '_ ` _ \\| | | | |\n"
                 + "|  _ < (_| | | | | | | | |_| |\n"
                 + "|_| \\_\\__,_|_| |_| |_|_|\\__, |\n"
                 + "                        |___/ \n";
-        System.out.println(banner);
+        System.out.println(line);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Ramly.");
+        System.out.println("What do you have in mind today?");
+        System.out.println(line);
+        System.out.println("I will take my leave now. Pleasure assisting you!");
+        System.out.println(line);
+
     }
 }
