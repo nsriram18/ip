3fdb7698eb822e4bd6429c90f73c36d5b167b907
N Sriram <narayanamurthysriram@gmail.com>
2026-08-19 23:25:06 +0800

    Editted new name and banner for Chatbot

---
+++ src/main/java/Ramly.java
@@ -0,0 +1,11 @@
+public class Ramly {
+    public static void main(String[] args) {
+        String banner = " ____                 _       \n"
+                + "|  _ \\ __ _ _ __ ___ | |_   _ \n"
+                + "| |_) / _` | '_ ` _ \\| | | | |\n"
+                + "|  _ < (_| | | | | | | | |_| |\n"
+                + "|_| \\_\\__,_|_| |_| |_|_|\\__, |\n"
+                + "                        |___/ \n";
+        System.out.println(banner);
+    }
+}
