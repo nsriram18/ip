8edeaf89c9f9502b06b8c66c02e6b4d0f9522f3f
N Sriram <narayanamurthysriram@gmail.com>
2026-08-19 23:59:42 +0800

    added echo and exit with bye

--- src/main/java/Ramly.java
+++ src/main/java/Ramly.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Ramly {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -12,8 +13,25 @@ public class Ramly {
         System.out.println("Hello! I'm Ramly.");
         System.out.println("What do you have in mind today?");
         System.out.println(line);
-        System.out.println("I will take my leave now. Pleasure assisting you!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println("I will take my leave now. Pleasure assisting you!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+        scanner.close();
 
     }
+
 }
