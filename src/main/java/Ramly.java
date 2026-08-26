5f06d36c401c3330c7e96b7bbb9704ad04aae5c0
N Sriram <narayanamurthysriram@gmail.com>
2026-08-27 01:45:17 +0800

    Encapsulate application loop in Ramly
    Add collaborators, constructor, and run method to Ramly so main only creates and starts the application, matching the command-oriented architecture.

--- src/main/java/Ramly.java
+++ src/main/java/Ramly.java
@@ -1,28 +1,34 @@
 public class Ramly {
-    private static final String FILE_PATH = "./data/ramly.txt";
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        Parser parser = new Parser();
+    private final Storage storage;
+    private final TaskList tasks;
+    private final Ui ui;
+    private final Parser parser;
+
+    public Ramly(String filePath) {
+        ui = new Ui();
+        parser = new Parser();
+        storage = new Storage(filePath);
+        tasks = new TaskList(storage.load());
+    }
+
+    public void run() {
         ui.showWelcome();
-
-        Storage storage = new Storage(FILE_PATH);
-        TaskList tasks = new TaskList(storage.load());
-
-        while (true) {
+        boolean isExit = false;
+        while (!isExit) {
             String input = ui.readCommand();
             ui.showLine();
-
             Command command = parser.parse(input);
-            if (command != null) {
-                command.execute(tasks, ui, storage);
-                if (command.isExit()) {
-                    break;
-                }
+            command.execute(tasks, ui, storage);
+            isExit = command.isExit();
+            if (!isExit) {
                 ui.showLine();
             }
-
         }
         ui.close();
+    }
+
+    public static void main(String[] args) {
+        new Ramly("./data/ramly.txt").run();
     }
 
 }
